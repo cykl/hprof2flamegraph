f4c5a1c415ec124efc81eb5513ba0f0d2e5be55f
cykl <clement@unportant.info>
2013-07-04 00:51:16 +0200

    Typofix: bad indentation

--- tests/test.py
+++ tests/test.py
@@ -69,7 +69,7 @@ class TestHeader(unittest.TestCase):
         header = "JAVA PROFILE 1.0.1, created Fri Jun 14 01:18:27 2013"
         self.assertTrue(header_match(header))
 
-	header = "JAVA PROFILE 1.0.1, created Wed Jul  3 20:50:41 2013"
+        header = "JAVA PROFILE 1.0.1, created Wed Jul  3 20:50:41 2013"
         self.assertTrue(header_match(header))
 
     def test_header_match_ko(self):
