c2e661df2ed870b35eefdd3e8dac5557b11b27ce
Clément MATHIEU <clement@unportant.info>
2017-03-29 21:27:30 +0200

    Prepare release 0.0.6

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ if sys.version_info < (2, 7):
 setup(
     name="hprof2flamegraph",
     description="Java Flame Graphs",
-    version="0.0.6-dev",
+    version="0.0.7-dev",
     author="Clément MATHIEU",
     author_email="clement+flamegraph@unportant.info",
     url="https://github.com/cykl/hprof2flamegraph",
